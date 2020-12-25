e6ba3112d764924375bbaf41b799236a3be5275d
shazy89 <erdo.shazy123@gmail.com>
2020-12-25 12:18:41 -0500

    store data inside the mongodb

--- src/index.js
+++ src/index.js
@@ -1,41 +1,37 @@
+require('./models/User');
+//require('./models/Track');
+require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser')
-const authRoutes = require('./routes/authRoutes')
-require('dotenv').config()
+const bodyParser = require('body-parser');
+
+const authRoutes = require('./routes/authRoutes');
+//const trackRoutes = require('./routes/trackRoutes');
+//const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
-
 app.use(bodyParser.json());
 app.use(authRoutes);
 
+//app.use(trackRoutes);
 
+const mongoUri = `mongodb+srv://ed:${process.env.DB_PASS}@cluster0.jybnz.mongodb.net/Cluster0?retryWrites=true&w=majority`;
 
+ mongoose.connect(mongoUri, {
+   useNewUrlParser: true,
+   useCreateIndex: true
+ });
+ mongoose.connection.on('connected', () => {
+   console.log('Connected to mongo instance');
+ });
+ mongoose.connection.on('error', err => {
+   console.error('Error connecting to mongo', err);
+ });
 
-const mongoUri = `mongodb+srv://ed:${process.env.DB_PASS}@cluster0.jybnz.mongodb.net/Cluster0?retryWrites=true&w=majority`
-
-mongoose.connect(mongoUri, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true
-});
-
-mongoose.connection.on('connected', ()=> {
-    console.log('Connected to mongo instance')
-})
-mongoose.connection.on('error', (err)=> {
-    console.log('Error connecting to mongo >>' + err)
-})
-
-app.get('/', (req, res) => {
-    res.send('Hi there!')
-});
-app.get('/hey', (req, res) => {
-    res.send('COOOOL HEY PAGEE')
-});
-
+//app.get('/', requireAuth, (req, res) => {
+//  res.send(`Your email: `);
+//});
 
 app.listen(3000, () => {
-    console.log(`Example app listening at http://localhost:3000`)
-    console.log('coool')
-  })
+  console.log('Listening on port 3000');
+});
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const mongose = require('mongoose');
+//to get access to the user model require mongose then
+const User = mongose.model('User');
+  // now we 
 const router = express.Router();
 
-router.post('/signup', (req, res) => {
-     console.log(req.body)
+router.post('/signup', async (req, res) => {
+    const { email, password } = req.body;
+
+   
+      const user = new User({ email, password });
+      await user.save();
+
     res.send('You made a post request')
 })
 
