564262cd4d25f1b21b8b20530e163ab059a2dfa8
shazy89 <erdo.shazy123@gmail.com>
2020-12-25 08:23:51 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hi there!')
+});
+
+app.listen(3000, () => {
+    console.log(`Example app listening at http://localhost:3000`)
+    console.log('coool')
+  })
