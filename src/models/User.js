234b47aa8f05b731dc1052642c7ae6ffea22f792
shazy89 <erdo.shazy123@gmail.com>
2020-12-25 13:41:14 -0500

    Using bycrpt salting and hashing

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt')
 const userSchema = new mongoose.Schema({
     email: {
       type: String,
@@ -11,5 +11,33 @@ const userSchema = new mongoose.Schema({
       required: true
     }
   });
+      // to be able to use  ( << this >> )  to we must use function(), if we use ( => )function we cannot use inside the arrow func  ( << this >> ) 
+  userSchema.pre('save', function(next) {
+     const user = this;
+     if(!user.isDirectModified('password')){
+         return next()
+     }
+     bcrypt.genSalt(10, (err, salt) => {
+       if (err) next(err);
+       bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) next(err); 
+        user.password = hash;
+        next();
+       }); 
+     });
+  });
 
-mongoose.model('User', userSchema);
+  userSchema.methods.comparePassword = function(candidatePassword) {
+      const user = this;
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+            if (err) reject(err); 
+            if (!isMatch) reject(false);
+            resolve(true);
+        });
+    });
+  }
+
+ mongoose.model('User', userSchema);
+
+      
