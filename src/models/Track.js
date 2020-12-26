570bc6841374b05630004a81de6684da58c3852e
shazy89 <erdo.shazy123@gmail.com>
2020-12-26 09:02:39 -0500

    Track models

---
+++ src/models/Track.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const pointSchema = new mongoose.Schema({
+    timestamp: Number,
+    coords: {
+        latitude: Number,
+        longitude: Number,
+        altitude: Number,
+        accuracy: Number,
+        heading: Number,
+        speed: Number
+    }
+});
+
+const trackSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // This is how we indicate that user id is reference to some other id stored in monog db
+        ref: 'User' // this is how mongoose will understand that user_id is poingint any instance of USER<<< ref: 'User"   >>>
+    },
+    name: {
+        type: String,
+        default: ''
+    },
+    locations: [pointSchema]
+})
